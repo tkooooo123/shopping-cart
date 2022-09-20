9f32400cb35408f25b60821ebe71ea83ffdc4ba4
tkooooo123 <tkooooo123@gmail.com>
2022-09-20 14:34:54 +0800

    add cartItem seed file

---
+++ seeders/20220920061532-cartItem-seed-file.js
@@ -0,0 +1,24 @@
+'use strict';
+const db = require('../models')
+const { Cart, Product } = db
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const carts = await Cart.findAll()
+    const products = await Product.findAll()
+    await queryInterface.bulkInsert('CartItems',
+      Array.from({ length: 10 }).map((v, i) => ({
+        cartId: carts[i % (carts.length)].id,
+        productId: products[Math.floor(Math.random() * 50)].id,
+        quantity: Math.floor(Math.random() * 50 + 10),
+        createdAt: new Date,
+        updatedAt: new Date
+      })),
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Cartitems', null, {})
+  }
+};
