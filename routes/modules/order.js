e3ca39f4b06a5b80019d99b027932b785fa6cc1e
tkooooo123 <tkooooo123@gmail.com>
2022-09-22 16:17:25 +0800

    add payment page

--- routes/modules/order.js
+++ routes/modules/order.js
@@ -5,5 +5,6 @@ const orderController = require('../../controllers/order-controller')
 router.get('/', orderController.getOrder)
 router.post('/', orderController.postOrder)
 router.put('/:id/cancel', orderController.cancelOrder)
+router.get('/:id/payment', orderController.getPayment)
 
 module.exports = router
