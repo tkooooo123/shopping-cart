58a939b38f09c3171ca014688816d1dcf1a68198
tkooooo123 <tkooooo123@gmail.com>
2022-09-23 17:03:48 +0800

    add edit product page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,75 +2,95 @@ const db = require('../models')
 const { Product, Category } = db
 const { localFileHandler } = require('../helpers/file-helpers')
 const adminController = {
-getProducts: async(req, res) => {
-    try {
-        const categoryId = Number(req.query.categoryId) || ''
-        const products = await Product.findAll({
-            include: Category ,
-            where: {
-                ...categoryId ? { categoryId } : {}
-            },
-            nest: true,
-            raw: true
-        })
-        const categories = await Category.findAll({
-            raw: true
-        })
-        return res.render('admin/products',{ 
-            products,
-            categories,
-            categoryId
-         })
-    } catch (error) {
-        console.log(error)
-    }
-},
-getProduct: async(req, res) => {
-    try {
-        const product = await Product.findByPk(req.params.id, {
-            raw: true
-        })
+    getProducts: async (req, res) => {
+        try {
+            const categoryId = Number(req.query.categoryId) || ''
+            const products = await Product.findAll({
+                include: Category,
+                where: {
+                    ...categoryId ? { categoryId } : {}
+                },
+                nest: true,
+                raw: true
+            })
+            const categories = await Category.findAll({
+                raw: true
+            })
+            return res.render('admin/products', {
+                products,
+                categories,
+                categoryId
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    getProduct: async (req, res) => {
+        try {
+            const product = await Product.findByPk(req.params.id, {
+                raw: true
+            })
 
-        return res.render('admin/product', {
-            product
-        })
-    } catch (error) {
-        console.log(error)
-    }
-},
-addProduct: async(req, res) => {
-    try {
-        const categories = await Category.findAll({
-            raw: true
-        })
+            return res.render('admin/product', {
+                product
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    addProduct: async (req, res) => {
+        try {
+            const categories = await Category.findAll({
+                raw: true
+            })
 
-        return res.render('admin/create-product', {
-            categories
-        })
+            return res.render('admin/create-product', {
+                categories
+            })
 
-    } catch (error) {
-        console.log(error)
-    }
-},
-postProduct: async(req, res) =>{
-    try {
-        const { name, categoryId, image, description, quantity, price } = req.body
-        const { file } = req
-        const filePath = await localFileHandler(file)
-        await Product.create({
-            name,
-            categoryId,
-            image: filePath || null,
-            description,
-            quantity,
-            price
-        })
-        return res.redirect('/admin/products')
-    } catch (error) {
-        console.log(error)
-    }
-}
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    postProduct: async (req, res) => {
+        try {
+            const { name, categoryId, image, description, quantity, price } = req.body
+            const { file } = req
+            const filePath = await localFileHandler(file)
+            await Product.create({
+                name,
+                categoryId,
+                image: filePath || null,
+                description,
+                quantity,
+                price
+            })
+            return res.redirect('/admin/products')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    editProduct: async (req, res) => {
+        try {
+            const product = await Product.findByPk(req.params.id, {
+                include: Category,
+                nest: true,
+                raw: true
+            })
+            const categories = await Category.findAll({
+                nest: true,
+                raw: true
+            })
+            console.log(product)
 
+            return res.render('admin/edit-product', {
+                product,
+                categories
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { editProduct } = require('../../controllers/admin-controller')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
@@ -8,7 +9,7 @@ router.get('/products', adminController.getProducts)
 router.get('/product/add', adminController.addProduct)
 router.get('/product/:id', adminController.getProduct)
 router.post('/product', upload.single('image'), adminController.postProduct)
-
+router.get('/product/:id/edit', adminController.editProduct)
 
 router.get('/', (req, res) => {
     res.redirect('/admin/products')
