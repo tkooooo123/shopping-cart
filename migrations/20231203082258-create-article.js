a47053f7da481fb585f8e11fe5d71069d6da36d3
tkooooo123 <tkooooo123@gmail.com>
2023-12-05 20:19:23 +0800

    修正 heroku 部署錯誤

--- migrations/20231203082258-create-article.js
+++ migrations/20231203082258-create-article.js
@@ -29,7 +29,7 @@ module.exports = {
         defaultValue: false,
       },
       tag: {
-        type: Sequelize.DataTypes.JSON,
+        type: Sequelize.JSON,
         allowNull: false,
       },
       createdAt: {
