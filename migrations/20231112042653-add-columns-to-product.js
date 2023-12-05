5d47d0afc075209b383596ec1944f2dd9888a6d5
tkooooo123 <tkooooo123@gmail.com>
2023-12-05 20:04:37 +0800

    修正 heroku 錯誤

--- migrations/20231112042653-add-columns-to-product.js
+++ migrations/20231112042653-add-columns-to-product.js
@@ -7,11 +7,11 @@ module.exports = {
         type: Sequelize.BOOLEAN,
         allowNull: false
       }),
-      //queryInterface.addColumn('Products','imagesUrl', {
-        //type: Sequelize.JSON,
-        //defaultValue: [] 
+      queryInterface.addColumn('Products','imagesUrl', {
+        type: Sequelize.DataTypes.JSON,
+        defaultValue: [] 
         
-      //}),
+      }),
       queryInterface.addColumn('Products','content', {
         type: Sequelize.STRING,
         allowNull: false
@@ -22,7 +22,7 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
     return Promise.all([
       queryInterface.removeColumn('Products', 'is_enabled'),
-     // queryInterface.removeColumn('Products', 'imagesUrl'),
+      queryInterface.removeColumn('Products', 'imagesUrl'),
       queryInterface.removeColumn('Products', 'content'),
     ])
   }
--- migrations/20231203082258-create-article.js
+++ migrations/20231203082258-create-article.js
@@ -29,7 +29,7 @@ module.exports = {
         defaultValue: false,
       },
       tag: {
-        type: Sequelize.JSON,
+        type: Sequelize.DataTypes.JSON,
         allowNull: false,
       },
       createdAt: {
